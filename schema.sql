dad422c679dd72f31b6d0780669c5f2085d79fb1
Skylar Dorn <skylardorn@Skylars-MacBook-Pro.local>
2022-07-19 19:11:12 -0700

    seeds sql page

--- schema.sql
+++ schema.sql
@@ -12,5 +12,16 @@ CREATE TABLE department(
 );
 
 CREATE TABLE role (
-    
-)
+    id INT PRIMARY KEY (id)
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL
+);
